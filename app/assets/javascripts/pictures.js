8fbfedfb9f9581563a2af30f83d170547832416f
David Jiang <davidjgrey@gmail>
2016-09-20 09:04:17 -0700

    tags and dropdowns

--- app/assets/javascripts/pictures.js
+++ app/assets/javascripts/pictures.js
@@ -1,6 +1,8 @@
+$(document).ready(function(){
+  APP.waldo.init();
+})
 
-
-APP = APP || {}
+var APP = APP || {}
 
 
 APP.waldo = (function(){
@@ -10,32 +12,57 @@ APP.waldo = (function(){
     this.y = y;
   }
 
+  var CHARACTERS = ["Waldo", "Wenda", "Odlaw", "Wizard Whitebeard", "Woof"];
+
   var WIDTH = '25';
 
   var init = function() {
-
+    setClickListeners();
+    setHoverListeners();
   }
 
   var setClickListeners = function() {
     $('img').on("click", onClick);
   }
 
-
-
+  var setHoverListeners = function(){
+    $('#image-container').mouseover(showTags);
+    $('#image-container').mouseout(hideTags);
+  }
 
   var onClick = function(event) {
     var x = event.pageX;
     var y = event.pageY;
     t = new Tag(x, y)
-    showTag(t);
+    placeTag(t);
   }
 
+  var showTags = function(){
+    $(".tag").show();
+  }
 
-  var showTag = function(tag) {
-    $tagDisplay = $('<div>')
+  var hideTags = function(){
+    $(".tag").hide();
+  }
+
+  var placeTag = function(tag) {
+    var $tagDisplay = $('<div>')
       .addClass("tag")
       .css({left: tag.x - WIDTH/2, top: tag.y - WIDTH/2})
+    $("#image-container").append($tagDisplay);
+    var dropdown = buildDropdown();
+    $tagDisplay.append(dropdown);
+  }
 
+  var buildDropdown = function(){
+    var $dropDown = $('<div>').css("background-color", "white");
+    var $list = $("<ul>");
+    CHARACTERS.forEach(function(character){
+      var $listItem = $("<li>").text(character);
+      $list.append($listItem);
+    })
+    $dropDown.append($list);
+    return $dropDown;
   }
 
   return {
@@ -47,4 +74,3 @@ APP.waldo = (function(){
 })();
 
 
-$
